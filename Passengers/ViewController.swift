04dd619b53fcddceb16edbd5e0a7bfac14d171af
Pavel2555 <86742948+Pavel2555@users.noreply.github.com>
2021-12-10 15:36:38 +0200

    Add files via upload

---
+++ Passengers/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Passengers
+//
+//  Created by Павел Бескоровайный on 10.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
+
